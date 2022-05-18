b5eb079b36c39e1a41f8daa1fa44a887357ad887
ukmumair1234 <104687241+ukmumair1234@users.noreply.github.com>
2022-05-18 09:48:27 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -25,7 +25,7 @@ def cloneNode(update, context):
             deleteMessage(context.bot, msg)
         except DirectDownloadLinkException as e:
             deleteMessage(context.bot, msg)
-            return sendMessage(str(e), context.bot, update.message)
+            return sendMessage(str(e), context.bot, update)
     if "drive.google.com" in link:
         gd = gdriveTools.GoogleDriveHelper()
         res, size, name, files = gd.clonehelper(link)
